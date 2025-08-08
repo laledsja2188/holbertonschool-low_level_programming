d8904a5584a2d7d34b72f78ce53cfc13cb8b77eb
laledsja2188 <ceferovalale343@gmail.com>
2025-08-08 20:25:11 +0400

    task

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_memcpy - Copies memory area.
+ * @n: bytes from memory area
+ * @src: memory area that we copy elements from here
+ * @dest: memory area that we paste elements to here
+ *
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
