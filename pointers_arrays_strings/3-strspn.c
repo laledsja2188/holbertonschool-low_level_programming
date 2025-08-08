029910bbbaf0dbd82b95c2d5c4ae6743c076cc22
laledsja2188 <ceferovalale343@gmail.com>
2025-08-08 20:47:24 +0400

    task

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,25 +9,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int i;
-    int found;
+	unsigned int count = 0;
+	int i;
+	int found;
 
-    while (*s)
-    {
-        found = 0;
-        for (i = 0; accept[i] != '\0'; i++)
-        {
-            if (*s == accept[i])
-            {
-                found = 1;
-                break;
-            }
-        }
-        if (!found)
-            break;
-        count++;
-        s++;
-    }
-    return (count);
+	while (*s)
+	{
+		found = 0;
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+			break;
+		count++;
+		s++;
+	}
+	return (count);
 }
