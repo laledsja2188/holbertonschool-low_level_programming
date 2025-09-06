7640638010c60a5619afe3093c9c303285708179
laledsja2188 <ceferovalale343@gmail.com>
2025-09-06 19:50:12 +0400

    task

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * /**
+ * _strncat - string birlesdirir 
+ * @dest: Destination string
+ * @src: Source string
+ * @n: nnnnnn
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '/0';
+
+	return (dest);
+}
