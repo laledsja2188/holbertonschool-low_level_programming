27aa9072f84ff5093b0fea39059b5b2620e37f2d
laledsja2188 <ceferovalale343@gmail.com>
2025-09-09 19:07:05 +0400

    fixed

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,24 +9,24 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    /* Traverse the linked list */
-    while (h != NULL)
-    {
-        /* If str is NULL, print [0] (nil) */
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            /* Print the length of string and the string itself */
-            printf("[%u] %s\n", h->len, h->str);
+	/* Traverse the linked list */
+	while (h != NULL)
+	{
+		/* If str is NULL, print [0] (nil) */
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			/* Print the length of string and the string itself */
+			printf("[%u] %s\n", h->len, h->str);
 
-        /* Move to the next node */
-        h = h->next;
-        count++;  /* Increment node count */
-    }
+		/* Move to the next node */
+		h = h->next;
+		count++;	/* Increment node count */
+	}
 
-    /* Return the total count of nodes */
-    return count;
+	/* Return the total count of nodes */
+	return (count);
 }
 
