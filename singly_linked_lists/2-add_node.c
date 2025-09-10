0b36def6e82b6ed7d154e80cc5284159b7ce0e43
laledsja2188 <ceferovalale343@gmail.com>
2025-09-10 21:59:40 +0400

    Implement add_node function and main file for linked list operations

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @str: The string to be duplicated and stored in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	/* Calculate the length of the string */
+	while (str[len])
+		len++;
+
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Duplicate the string */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	/* Set the length and next pointer */
+	new_node->len = len;
+	new_node->next = *head;
+
+	/* Update head to point to the new node */
+	*head = new_node;
+
+	return (new_node);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -38,5 +38,14 @@ size_t print_list(const list_t *h);
  */
 size_t list_len(const list_t *h);
 
+/**
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: Pointer to a pointer to the head of the list
+ * @str: String to be duplicated and stored in the new node
+ *
+ * Return: The address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str);
+
 #endif /* LISTS_H */
 
