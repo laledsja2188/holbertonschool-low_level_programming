0214db217fb15c14d40c4712c0f595beb22e867a
laledsja2188 <ceferovalale343@gmail.com>
2025-09-10 21:45:29 +0400

    Add list_len function and main file for linked list operations

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -30,5 +30,13 @@ typedef struct list_s
  */
 size_t print_list(const list_t *h);
 
+/**
+ * list_len - Returns the number of elements in a linked list_t list
+ * @h: Pointer to the head of the list_t list
+ *
+ * Return: The number of elements in the list
+ */
+size_t list_len(const list_t *h);
+
 #endif /* LISTS_H */
 
