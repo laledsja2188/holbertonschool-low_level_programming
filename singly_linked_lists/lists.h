c0f1714225dc3225d1479074b17b05a82fa5739a
laledsja2188 <ceferovalale343@gmail.com>
2025-09-11 18:44:59 +0400

    Add add_node_end function and main file for linked list operations

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -47,5 +47,14 @@ size_t list_len(const list_t *h);
  */
 list_t *add_node(list_t **head, const char *str);
 
+/**
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: Pointer to a pointer to the head of the list
+ * @str: String to be duplicated and stored in the new node
+ *
+ * Return: The address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str);
+
 #endif /* LISTS_H */
 
