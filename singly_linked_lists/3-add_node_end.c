4c1d54b7d52f2d2a2d002adc6e13ca79adad98a7
lala <ceferovalale343@gmail.com>
2025-09-11 19:03:50 +0400

    task

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -34,23 +34,19 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	/* Set the length and next pointer */
 	new_node->len = len;
 	new_node->next = NULL;
-
-	/* If the list is empty, make new_node the head */
+	
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
-	/* Traverse to the end of the list */
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
 
-	/* Link the new node at the end */
 	temp->next = new_node;
 
 	return (new_node);
