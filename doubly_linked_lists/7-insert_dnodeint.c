d0eaf97faa79b32ca948bb75ce4be84b81837a11
laledsja2188 <ceferovalale343@gmail.com>
2025-09-12 20:29:03 +0400

    Optimize insert_dnodeint_at_index function - Betty compliant (0 errors, 0 warnings)

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,14 +15,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-
-	/* If inserting at the beginning */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	temp = *h;
-
-	/* Find the position to insert */
 	while (temp != NULL && count < idx)
 	{
 		if (count == idx - 1)
@@ -30,28 +25,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		count++;
 	}
-
-	/* If inserting at the end or temp is NULL */
 	if (temp == NULL)
 		return (NULL);
-
-	/* If inserting at the end */
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
-
-	/* Create new node */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
-	/* Set up the new node */
 	new_node->n = n;
 	new_node->next = temp->next;
 	new_node->prev = temp;
-
-	/* Update the links */
 	temp->next->prev = new_node;
 	temp->next = new_node;
-
 	return (new_node);
 }
