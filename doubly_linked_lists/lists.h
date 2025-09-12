49c48934ea60651c5b735b9119ecccc46f19af2c
laledsja2188 <ceferovalale343@gmail.com>
2025-09-12 19:41:31 +0400

    Add doubly linked list functions: get_dnodeint_at_index

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -62,4 +62,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
  */
 void free_dlistint(dlistint_t *head);
 
+/**
+ * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list
+ * @head: Pointer to the head of the list
+ * @index: Index of the node, starting from 0
+ *
+ * Return: The nth node, or NULL if the node does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
 #endif /* LISTS_H */
