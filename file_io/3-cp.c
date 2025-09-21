76a337b1b6fad7f7387346c7c8e795130989e7ac
laledsja2188 <ceferovalale343@gmail.com>
2025-09-21 21:37:57 +0400

    file_io: Fix critical logic issues in 3-cp.c
    - Fix file opening order: check file descriptors immediately after opening
    - Remove incorrect loop logic that reopened destination file multiple times
    - Add proper error checking after each file operation
    - Change from do-while to while loop for cleaner logic
    - Ensure buffer memory is freed on all error paths
    - Fix write error checking (remove redundant to == -1 check)
    
    These changes resolve the error-prone behavior where:
    1. Files were opened in wrong order
    2. Destination file was repeatedly opened in append mode
    3. Error checking happened too late after operations

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -68,12 +68,30 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
+	
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
 
-	do {
-		if (from == -1 || r == -1)
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[2]);
+		free(buffer);
+		exit(99);
+	}
+
+	r = read(from, buffer, 1024);
+	
+	while (r > 0)
+	{
+		if (r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
@@ -82,7 +100,7 @@ int main(int argc, char *argv[])
 		}
 
 		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		if (w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
@@ -91,9 +109,7 @@ int main(int argc, char *argv[])
 		}
 
 		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (r > 0);
+	}
 
 	free(buffer);
 	close_file(from);
