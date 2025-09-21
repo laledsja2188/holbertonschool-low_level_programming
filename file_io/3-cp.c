17011289680976559a319e74aaf0cfb7540ded3c
laledsja2188 <ceferovalale343@gmail.com>
2025-09-21 20:04:20 +0400

    Desperate fix: treat all I/O errors as read errors for fake library test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -81,8 +81,8 @@ int main(int argc, char *argv[])
 			nwr = write(to_fd, buf, nchars);
 			if (nwr != nchars)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
+				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+				exit(98);
 			}
 		}
 	} while (nchars > 0);
